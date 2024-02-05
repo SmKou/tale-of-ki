d868734c238219a2d14d9596ffc6b0ef27a61c39
Sm Kou <66178856+SmKou@users.noreply.github.com>
2024-02-05 01:24:41 -0800

    Change to Tale of Ki (series for family of Kibakhan)

--- README.md
+++ README.md
@@ -11,9 +11,27 @@ Text Adventure
 
 ## Description
 
-Sometimes, mistakes are inevitable. Whether conducted by stupidity, shame, a desire to belong, or anger, they happen and so, one must bear the consequences. More often though, life's consequences are strange and seemingly irrelevant. Hardly coherent enough to learn any lesson. [Insert a name] made a mistake. Now, they find themselves in the employ of a gremlin a few inches short of two feet, but says they are because of the point of their giant ears, wearing an oversized robe with many pockets who has a maze to traverse. The contract has been signed in blood. 
+Collection of text adventures featuring the family members from a family of gremlins, the Ki.
+
+**Tale of Kibakhan**:
+
+## Summaries
+
+### Tale of Kibakhan
+
+Sometimes, mistakes are inevitable. Whether conducted by stupidity, shame, a desire to belong, or anger, they happen and so, one must bear the consequences. More often though, life's consequences are strange and seemingly irrelevant. Hardly coherent enough to learn any lesson. 
+
+[Insert a name] made a mistake. Now, they find themselves in the employ of a gremlin a few inches short of two feet, but says they are because of the point of their giant ears, wearing an oversized robe with many pockets who has a maze to traverse. The contract has been signed in blood. 
 
 Get ready. Kibakhan commands you.
+
+### Tale of Kibikuun
+
+What is a story? No matter what the medium, a story can be found anywhere, telling itself over the course of a book, a game, or series. But if stories can be found anywhere and can form from the very experience of a story, it can be confusing who a story belongs to, right?
+
+Kibikuun has a story. They are making their way to see their cousin, Kibakhan, in Final City, but to get there, they're going to need help and perhaps a ride. Which unfortunate souls will help Kibikuun to Final City? Remember, this isn't your story, but you're going to forget that soon enough.
+
+Go! Join Kibikuun on their quest!
 
 ## Complete Setup
 
--- app/main.js
+++ app/main.js
@@ -1,151 +1,22 @@
-const main = {
-    ctrl: {
-        act: document.getElementById('act-btn'),
-        stats: document.getElementById('stats-btn'),
-        bag: document.getElementById('bag-btn'),
-        minmax: document.querySelector('.minmax-btn'),
-        set: document.getElementById('set-btn')
-    },
-    ui: {
-        text: document.querySelector('.ui-text'),
-        ipt: {
-            sec: document.querySelector('.ui-ipt'),
-            text: document.querySelector('.ui-ipt textarea'),
-            btn: document.querySelector('.ui-ipt button')
-        },
-        aside: document.querySelector('aside'),
-        stats: document.querySelector('.ui-stats'),
-        bag: document.querySelector('.ui-bag'),
-        act: document.querySelector('.ui-act'),
-        set: document.querySelector('.set')
+const ui = {
+    iaa: {
+        ipt: document.getElementById('iaa-ipt'),
+        disp: document.getElementById('iaa-name'),
+        warn: document.querySelector('.iaa-warn')
     }
 }
 
-const settings = {
-    switches: document.querySelectorAll('.setting .toggle-switch'),
-    on: document.querySelectorAll('.toggle-switch .on'),
-    off: document.querySelectorAll('.toggle-switch .off'),
-    val: {
-        'splash-screen': false,
-        'title-color': true, // true = changing color
-        'color-mode': true, // true = dark
-        'mem-access': true, // true = use local storage
-        'narrator': true // true = kibakhan
-    },
-    storage: ''
-}
+ui.iaa.ipt.addEventListener('change', e => {
+    e.preventDefault()
 
-
-
-const splash_screen = document.querySelector('.splash-screen')
-const enter_btn = document.getElementById('enter')
-
-enter_btn.addEventListener('click', () => {
-    splash_screen.classList.add('collapsed')
-    settings.val['splash-screen'] = true
-})
-
-const toggle = e => {
-    const sec = e.target.innerHTML.toLowerCase()
-    console.log(main.ui[sec].classList, main.ui.aside.classList)
-    if (main.ui[sec].classList.contains('collapsed')) {
-        main.ui[sec].classList.remove('collapsed')
-        main.ui.aside.classList.remove('collapsed')
+    const val = e.target.value
+    if (!/\d+/.test(val)) {
+        ui.iaa.warn.classList.remove('no-disp')
+        ui.iaa.warn.innerHTML = 'Invalid name'
+        return false
     }
-    else {
-        main.ui[sec].classList.add('collapsed')
     
-        let allCollapsed = true
-        document.querySelectorAll('aside section').forEach(section => {
-            if (!section.classList.contains('collapsed'))
-                allCollapsed = false
-        })
-
-        console.log(allCollapsed)
-        if (allCollapsed)
-            main.ui.aside.classList.remove('collapsed')
-    }
-}
-
-main.ui.ipt.btn.addEventListener('click', () => {
-    const val = main.ui.ipt.text.value
-    const p = document.createElement('p')
-    p.setAttribute('class', 'player speech')
-    p.append(document.createTextNode(val))
-    main.ui.text.append(p)
-    main.ui.ipt.text.value = ''
+    ui.iaa.warn.classList.add('no-disp')
+    ui.iaa.disp.innerHTML = val
+    localStorage.setItem('iaa-name', val)
 })
-
-main.ctrl.act.addEventListener('click', toggle)
-main.ctrl.stats.addEventListener('click', toggle)
-main.ctrl.bag.addEventListener('click', toggle)
-main.ctrl.set.addEventListener('click', toggle)
-
-main.ctrl.minmax.addEventListener('click', e => {
-    const [, mode] = e.target.classList
-    if (mode === 'max') {
-        e.target.classList.remove('max')
-        e.target.classList.add('min')
-        document.documentElement.requestFullscreen()
-    }
-    else {
-        e.target.classList.remove('min')
-        e.target.classList.add('max')
-        document.exitFullscreen()
-    }
-})
-
-main.ui.ipt.text.addEventListener('keydown', e => {
-    if (e.key === "Enter") {
-        e.preventDefault()
-        main.ui.ipt.btn.click()
-    }
-})
-
-/* --------------------------------------------- Settings */
-
-// If settings disabled (localstorage inaccessible)
-
-for (let i = 0; i < settings.on.length; ++i) {
-    const eSwitch = settings.switches[i]
-    settings.on[i].addEventListener('click', () => {
-        const [, s, mode] = eSwitch.classList
-        if (mode) return;
-        eSwitch.classList.toggle('on')
-        switch (s) {
-            case 'title-color':
-                document.querySelector('header h1 .title-lead').classList.add('rainbow')
-                break
-            case 'color-mode':
-                document.querySelector('body').classList.remove('light')
-                document.querySelector('body').classList.add('dark')
-                break
-            case 'mem-access':
-                break
-            case 'narrator':
-                break
-        }
-        settings.val[s] = true
-    })
-    settings.off[i].addEventListener('click', () => {
-        const [, s, mode] = eSwitch.classList
-        if (!mode) return;
-        eSwitch.classList.toggle('on')
-        switch (s) {
-            case 'title-color':
-                document.querySelector('header h1 .title-lead').classList.remove('rainbow')
-                break
-            case 'color-mode':
-                document.querySelector('body').classList.remove('dark')
-                document.querySelector('body').classList.add('light')
-                break
-            case 'mem-access':
-                break
-            case 'narrator':
-                break
-        }
-        settings.val[s] = false
-    })
-}
-
-// Save to localstorage
